145ac2ce332c96b5015b070a15d54189b9324ae4
NANCIRA <84852048+NANCIRA@users.noreply.github.com>
2021-07-31 22:59:54 -0500

    sevent commit guardo cambios correcion espacios  y variables en html y js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// Variables globales
+//Estableciendo variables
 
 const formularioUI =  document.querySelector('#formulario');
-const listaPolizaUI = document.getElementById('listaPoliza');
+const tipoPolizaUI = document.getElementById('tipoPoliza');
 let arrayPoliza = [];
 
-// Funciones
-
-
+// Creando las funciones
 const CrearItem = (poliza) => {
 
     let item = {
@@ -28,84 +26,76 @@ const GuardarDB = () => {
 
 const LeerDB = () => {
 
-    listaPolizaUI.innerHTML = '';
+    tipoPolizaUI.innerHTML = '';
 
     arrayPoliza = JSON.parse(localStorage.getItem('productos'));
 
     if(arrayPoliza === null){
         arrayPoliza = [];
     }else{
+     arrayPoliza.forEach(element => {
 
-
-        arrayPoliza.forEach(element => {
-            listaPolizaUI.innerHTML += `<div class="alert alert-danger" role="alert">
-             <span class="material-icons" float-left mr-3>article</span><b>${element.poliza}</b> - ${element.estado} <span class="float-right">
-             <i class="material-icons">done</i><i class="material-icons">delete</i></span></div>`
-            });
+       if(element.estado === true){
+        tipoPolizaUI.innerHTML += `<div class="alert alert-primary" role="alert">
+        <span class="material-icons" float-left mr-3>article</span><b>${element.poliza}</b> - ${element.estado} <span class="float-right">
+        <i class="material-icons">done</i><i class="material-icons">delete</i></span></div>`   
+      }else{
+        tipoPolizaUI.innerHTML += `<div class="alert alert-danger" role="alert">
+        <span class="material-icons" float-left mr-3>article</span><b>${element.poliza}</b> - ${element.estado} <span class="float-right">
+        <i class="material-icons">done</i><i class="material-icons">delete</i></span></div>`
+      }
+      });
     }
-
 }
 const EliminarDB = (poliza) => {
-    let indexArray;
-    arrayPoliza.forEach((elemento, index) => {
+  let indexArray;
+   arrayPoliza.forEach((elemento, index) => {
 
-        if(elemento.poliza === poliza){
-            indexArray = index;
-        }
-    });
+   if(elemento.poliza === poliza){
+    indexArray = index;
+}
+});
 
-    arrayPoliza.splice(indexArray,1);
-    GuardarDB();
-
+  arrayPoliza.splice(indexArray,1);
+  GuardarDB();
 }
 
 const EditarDB = (poliza) => {
     
-    let indexArray = arrayPoliza.findIndex((elemento)=>elemento.poliza ===
-        poliza);
+ let indexArray = arrayPoliza.findIndex((elemento)=>{
+     return elemento.poliza ===  poliza});
 
-       arrayPoliza[indexArray].estado = true;
-
-       GuardarDB();
+  arrayPoliza[indexArray].estado = true;
+  GuardarDB();
 }
-
-
 
 // EvenListener
 
 formularioUI.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let polizaUI = document.querySelector('#poliza').value;
-º   
+ e.preventDefault();
+ let polizaUI = document.querySelector('#poliza').value;
+
     
-    CrearItem(polizaUI);
-    GuardarDB();
+ CrearItem(polizaUI);
+ GuardarDB();
 
-
-
-    formularioUI.reset();
-
-
+ formularioUI.reset();
 });
 
 document.addEventListener('DOMContentLoaded', LeerDB);
 
-listaPolizaUI.addEventListener('click', (e) => {
+tipoPolizaUI.addEventListener('click', (e) => {
+ e.preventDefault();
 
-    e.preventDefault();
-
-      if(e.target.innerHTML === 'done' || e.target.innerHTML === 'delete'){
-        let texto = e.path[2].childNodes[2].innerHTML;
-        if(e.target.innerHTML === 'delete'){
-            //Acción de eliminar
-            EliminarDB(texto);
+  if(e.target.innerHTML === 'done' || e.target.innerHTML === 'delete'){
+   
+  if(e.target.innerHTML === 'delete')
+       //Eliminar
+            EliminarDB(e.path[2].childNodes[2].innerHTML);
         }
        if(e.target.innerHTML === 'done'){
-           //Acción de editar
-
+           //Editar
+           EditarDB(e.path[2].childNodes[2].innerHTML);
        }
-
-    }
-
 
 });
